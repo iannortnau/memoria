47fa587657130753605be25bd3bbf3fd16627d95
iannocsantos <iann_ortnau@hotmail.com>
2021-07-22 15:31:22 -0300

    0.4.0.0 /Implemantando next auht

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,16 @@ import '../styles/w3.css'
 import '../styles/animate.css'
 import '../styles/all.css'
 import ContextoHome from "../context/contexto"
+import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
-  return (
-      <ContextoHome>
-        <Component {...pageProps} />
-      </ContextoHome>
-  );
+    return (
+        <Provider session={pageProps.session}>
+            <ContextoHome>
+                <Component {...pageProps} />
+            </ContextoHome>
+        </Provider>
+    );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,35 @@ import {ContextoGlobal} from "../context/contexto";
 import {useContext} from "react";
 import Tabuleiro from "../components/Tabuleiro";
 import Carregando from "../components/Carregando";
+import { signIn, signOut, useSession } from 'next-auth/client';
+
+
 
 
 
 export default function Home() {
+    const [ session, loading ] = useSession();
     const contexto = useContext(ContextoGlobal);
+    //TODO: APRENDER A USAR O PRISMA https://www.prisma.io/nextjs
 
 
     switch (contexto.state.estado) {
         case 0:
             return (<PopInicial/>);
         case 1:
-            return(<Tabuleiro/>);
+            return(
+                <>
+                    {!session && <>
+                        Not signed in <br/>
+                        <button onClick={() => signIn()}>Sign in</button>
+                    </>}
+                    {session && <>
+                        Signed in as {session.user.email} <br/>
+                        <button onClick={() => signOut()}>Sign out</button>
+                    </>}
+                    <Tabuleiro/>
+                </>
+            );
         case 2:
             setTimeout(function (){contexto.setState({...contexto.state, nCartas: contexto.state.nCartas+2, estado: 1})},2000);
             return(<Carregando/>);
