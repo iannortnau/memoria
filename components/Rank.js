b01be80a3b0931fe9a3656042265e1769e413b15
iannocsantos <iann_ortnau@hotmail.com>
2021-07-25 19:42:49 -0300

    0.5.0.4/Ajuste de estilo

--- components/Rank.js
+++ components/Rank.js
@@ -44,7 +44,7 @@ export default function Rank(props) {
             <div className={"w3-center w3-bar-block w3-collapse w3-card-4 w3-light-purple w3-text-deep-purple w3-round-xxlarge w3-large w3-animate-right "+styles.rank} style={{width: "300px",right:0}} id="mySidebar">
                 <span className={"w3-card w3-bar-item w3-ripple "+styles.botao} onClick={function (){setEstado(!estado);}}>Fechar <b>X</b></span>
                 <span className={"w3-bar-item w3-ripple "+styles.botao} onClick={function (){setEstado(!estado);}}><b>Rank</b></span>
-                <div style={{overflow: "auto"}}>
+                <div className={styles.rankLista}>
                     {(rank.length === 0)
                         ?
                         <div className={"w3-margin w3-round-xxlarge w3-padding "+styles.item} style={{backgroundColor: "#d7cbec"}}>
